0beced3a6ac4602769800e5c3b354a449a6f8259
lisazeyen <35347358+lisazeyen@users.noreply.github.com>
2018-01-16 15:37:54 +0100

    etwas kommentiert und stroke_counter vervollständigt

--- scr/inoutcsv/obj_writer.py
+++ scr/inoutcsv/obj_writer.py
@@ -1,11 +1,7 @@
-
+"""module which contains the class AS, which saves all the statistic data in one object"""
 
 class AS():
 
     def __init__(self, word_count, stroke_count):
         self.word_count = word_count
         self.stroke_count = stroke_count
-        
-       
-
-        
