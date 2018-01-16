1ca6459f4b348c97f3b9466c6905f8d911e6e90c
lisazeyen <35347358+lisazeyen@users.noreply.github.com>
2018-01-16 18:02:52 +0100

    etwas kommentiert und stroke_counter vervollständigt

--- scr/inoutcsv/file_handler.py
+++ scr/inoutcsv/file_handler.py
@@ -1,4 +1,4 @@
-"""Module which handles
+"""Module which handles the GUI
 """
 
 
@@ -12,16 +12,17 @@ __credits__ = ""
 __email__ = "uni.goethe.horde@gmail.com"
 
 
-class Handler():
+class Handler:
     read_data = None
     fname = "Morgen_Kinder.txt"
     UTF8_fname = "UTF8_Morgen_Kinder.txt"
     filename = ""
 
     def __init__(self):
+        """class inisialization"""
         self.root = tk.Tk()
         self.root.withdraw()
-        self.filename = askopenfilename()
+        self.filename = askopenfilename()   # creates a file dialog object
         if self.filename == "":
             print("No file chosen")
         else:
@@ -29,28 +30,32 @@ class Handler():
             self.read_file()
     
     def read_file(self):
+        file_encoding = " "
         test_file = self.filename
-        bom_dict = {
+        # dictionnary to test for different coding systems
+        encode_dict = {
             codecs.decode(codecs.BOM_UTF8, "cp1252"): "UTF-8-SIG",
             codecs.decode(codecs.BOM_UTF16_LE, "cp1252"): "UTF-16",
         }
 
         f = open(test_file, "r")
-        temp = f.readline()
+        temp = f.readline()       # save the first line in an string temp
         f.close()
 
-        for key in bom_dict:
+        for key in encode_dict:
             if temp.startswith(key):  # check for empty lines
-                file_encoding = bom_dict.get(key)
-                # print("Codec found", bom_dict.get(key), "\n")
+                file_encoding = encode_dict.get(key)  # determines, which encoding is used
+                print("Codec found", encode_dict.get(key), "\n")   # for testing
                 break
             f.close()
 
         try:
             with open(test_file, "r", encoding=file_encoding, errors="surrogateescape",
                       buffering=1) as f:
+                # buffering=1 means line buffered, errors = 'surrogateescape' replace byte to
+                # encoding code, if error, turn back to same byte
                 self.read_data = f.read()
             f.close()
             return self.read_data
-        except FileNotFoundError:
+        except FileNotFoundError:  # if file/directory is requested which doesn't exist
             return 0
--- scr/inoutcsv/main.py
+++ scr/inoutcsv/main.py
@@ -24,15 +24,18 @@ def error_code(n):
 def main():
     """main function"""
     file_handler_obj = Handler()
-    content = file_handler_obj.read_file()
-    if content == 0:    # chosen file not found
-        error_code(0)
+    if file_handler_obj.filename != "":
+        content = file_handler_obj.read_file()
+        if content == 0:    # chosen file not found
+            error_code(0)
+        else:
+            print(content)
+            json_obj = analyzer.analyzer(content)
+            parsed = json.loads(json_obj)
+            print(json.dumps(parsed, indent=4, sort_keys=True,
+                             ensure_ascii=False))  # encoding
     else:
-        print(content)
-        json_obj = analyzer.analyzer(content)
-        parsed = json.loads(json_obj)
-        print(json.dumps(parsed, indent=4, sort_keys=True,
-                         ensure_ascii=False))  # encoding
+        print("Programm wird abgebrochen")
 
 
 # -------- MAIN -----------------------------------------------------------------------------------
