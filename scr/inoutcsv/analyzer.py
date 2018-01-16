2b1676f5a9ae6db12d2a1d4138c51b4d3063215d
lisazeyen <35347358+lisazeyen@users.noreply.github.com>
2018-01-16 19:13:01 +0100

    jetzt zählt er auch 155 Wörter

--- scr/inoutcsv/analyzer.py
+++ scr/inoutcsv/analyzer.py
@@ -11,10 +11,11 @@ def analyzer(file_data):
     word_list = file_data.split()   # splits the text file into words
     print(word_list)   # for testing
     print(len(word_list))
-    stroke_count_n = stroke_count(word_list)  # counts the number of key strokes
+    stroke_count_n = stroke_count(file_data)  # counts the number of key strokes
     word_list = string_cleaner(word_list)     # removes the special characters of the word list
     word_count_n = word_count(word_list)      # calculates the number of words in the list
-    statistic_obj = obj_writer.AS(word_count_n, stroke_count_n)  # saves the data in an object
+    word_stat = word_frequency_distribution(word_list)
+    statistic_obj = obj_writer.AS(word_count_n, stroke_count_n, word_stat)  # saves the data in an object
     json_obj = json.dumps(statistic_obj.__dict__)   # turns the object to a JSON object
     return json_obj
 
@@ -46,8 +47,15 @@ def char_frequency_distribution():
     pass
 
 
-def word_frequency_distribution():
-    pass
+def word_frequency_distribution(word_list):
+    num_words = len(word_list)   # number of words in the text
+    words_stat_relativ = {}      # dictionary for relative frequency
+    words_stat = {i: word_list.count(i) for i in word_list}   # dictionary with total frequency
+    for key in words_stat:
+        average = words_stat[key] * 100 / num_words
+        words_stat_relativ.update({key: average})
+    print(words_stat_relativ)
+    return words_stat_relativ
 
 
 def mean_word_length():
@@ -55,13 +63,17 @@ def mean_word_length():
     
     
 def string_cleaner(word_list):
-    """function, which takes as a parameter a list and removes '!', '.', ',' """
+    """function, which takes as a parameter a list and removes '!', '.', ',', '-' """
+    word_list2 = []
     for i in range(len(word_list)):
         if word_list[i].endswith("!"):
             word_list[i] = word_list[i].replace("!", "")
-        if word_list[i].endswith("."):
+        elif word_list[i].endswith("."):
             word_list[i] = word_list[i].replace(".", "")
-        if word_list[i].endswith(","):
+        elif word_list[i].endswith(","):
             word_list[i] = word_list[i].replace(",", "")
-    print(word_list)   # for testing
-    return word_list
+    for i in range(len(word_list)):   # to remove '-' from the word list
+        if word_list[i] != "–":
+            word_list2.append(word_list[i])
+    print(word_list2)   # for testing
+    return word_list2
--- scr/inoutcsv/obj_writer.py
+++ scr/inoutcsv/obj_writer.py
@@ -2,6 +2,7 @@
 
 class AS():
 
-    def __init__(self, word_count, stroke_count):
+    def __init__(self, word_count, stroke_count, word_stat_relativ):
         self.word_count = word_count
         self.stroke_count = stroke_count
+        self.word_stat_relativ = word_stat_relativ
